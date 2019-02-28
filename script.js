d329b232ae8b3963231cbc0ac1c780d15388ef5b
Bmartinez24601 <43424534+Bmartinez24601@users.noreply.github.com>
2019-02-27 21:15:18 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,33 @@
+$("button").click(function() {
+    var numTyped = $(".theYear").val();
+    var numOfYears = numTyped-2019;
+    $(".p").append("In "+ numOfYears +" years, ");
+     var heroName = $(".theHero").val();
+    $(".p").append(heroName +" will be killed by ");
+    var villainName = $(".theVillain").val();
+    $(".p").append(villainName+".");
+    $(".surprise").text("or wil he/she???😉 ");
+});
+
+
+
+
+
+
+
+
+
+
+$("button").click(function() {
+    var age= $(".age").val();
+    var age2= age+3;
+    $("p").append(age2);
+});
+
+
+
+
+
+
+
+
